4eacf43d724118c5dbd90f087813475b77ff78f0
robin moussu <robin.moussu+git@gmail.com>
2014-10-11 03:36:04 +0000

    convert coding style

--- src/ensishell.c
+++ src/ensishell.c
@@ -15,40 +15,48 @@
 #error "Variante non défini !!"
 #endif
 
-int main() {
-        printf("Variante %d: %s\n", VARIANTE, VARIANTE_STRING);
+int main()
+{
+    printf("Variante %d: %s\n", VARIANTE, VARIANTE_STRING);
 
-	while (1) {
-		struct cmdline *l;
-		int i, j;
-		char *prompt = "ensishell>";
+    while (1) {
+        struct cmdline *l;
+        int i, j;
+        char *prompt = "ensishell>";
 
-		l = readcmd(prompt);
+        l = readcmd(prompt);
 
-		/* If input stream closed, normal termination */
-		if (!l) {
-			printf("exit\n");
-			exit(0);
-		}
+        /* If input stream closed, normal termination */
+        if (!l) {
+            printf("exit\n");
+            exit(EXIT_SUCCESS);
+        }
 
-		if (l->err) {
-			/* Syntax error, read another command */
-			printf("error: %s\n", l->err);
-			continue;
-		}
+        if (l->err) {
+            /* Syntax error, read another command */
+            printf("error: %s\n", l->err);
+            continue;
+        }
 
-		if (l->in) printf("in: %s\n", l->in);
-		if (l->out) printf("out: %s\n", l->out);
-		if (l->bg) printf("background (&)\n");
+        if (l->in) {
+            printf("in: %s\n", l->in);
+        }
+        if (l->out) {
+            printf("out: %s\n", l->out);
+        }
+        if (l->bg) {
+            printf("background (&)\n");
+        }
 
-		/* Display each command of the pipe */
-		for (i=0; l->seq[i]!=0; i++) {
-			char **cmd = l->seq[i];
-			printf("seq[%d]: ", i);
-                        for (j=0; cmd[j]!=0; j++) {
-                                printf("'%s' ", cmd[j]);
-                        }
-			printf("\n");
-		}
-	}
+        /* Display each command of the pipe */
+        for (i = 0; l->seq[i] != 0; i++) {
+            char **cmd = l->seq[i];
+
+            printf("seq[%d]: ", i);
+            for (j = 0; cmd[j] != 0; j++) {
+                printf("'%s' ", cmd[j]);
+            }
+            printf("\n");
+        }
+    }
 }
--- src/readcmd.h
+++ src/readcmd.h
@@ -17,26 +17,26 @@
 #define USE_GNU_READLINE
 
 /* Read a command line from input stream. Return null when input closed.
-Display an error and call exit() in case of memory exhaustion. */
+   Display an error and call exit() in case of memory exhaustion. */
 struct cmdline *readcmd(char *prompt);
 
 
 /* Structure returned by readcmd() */
 struct cmdline {
-	char *err;	/* If not null, it is an error message that should be
-			   displayed. The other fields are null. */
-	char *in;	/* If not null : name of file for input redirection. */
-	char *out;	/* If not null : name of file for output redirection. */
-        int   bg;       /* If set the command must run in background */ 
-	char ***seq;	/* See comment below */
+    char *err;			/* If not null, it is an error message that should be
+                           displayed. The other fields are null. */
+    char *in;			/* If not null : name of file for input redirection. */
+    char *out;			/* If not null : name of file for output redirection. */
+    int bg;			/* If set the command must run in background */
+    char ***seq;		/* See comment below */
 };
 
 /* Field seq of struct cmdline :
-A command line is a sequence of commands whose output is linked to the input
-of the next command by a pipe. To describe such a structure :
-A command is an array of strings (char **), whose last item is a null pointer.
-A sequence is an array of commands (char ***), whose last item is a null
-pointer.
-When the user enters an empty line, seq[0] is NULL.
-*/
+   A command line is a sequence of commands whose output is linked to the input
+   of the next command by a pipe. To describe such a structure :
+   A command is an array of strings (char **), whose last item is a null pointer.
+   A sequence is an array of commands (char ***), whose last item is a null
+   pointer.
+   When the user enters an empty line, seq[0] is NULL.
+   */
 #endif
